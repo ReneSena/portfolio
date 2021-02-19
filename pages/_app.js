b519c9865550fef94c9055b62c79dcaac5156045
ReneSena <rene.sena3005@gmail.com>
2021-02-18 23:26:51 -0300

    Adição de fontes e bibliotecas como styled-icons e normalize.

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,25 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+import { ThemeProvider } from "styled-components";
+import Head from "next/head";
+import theme from "../src/theme";
+import Reset from "../src/components/foundation/Reset";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Rene Sena | Portfolio</title>
+
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+
+				<link
+					href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600;700&family=Lato:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<ThemeProvider theme={theme}>
+				<Reset />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
