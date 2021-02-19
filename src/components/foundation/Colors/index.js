3759dbda58a33926c9e7c8b09a4d2d4d48db8534
ReneSena <rene.sena3005@gmail.com>
2021-02-18 23:25:33 -0300

    Criação do theme inicial do projeto.

---
+++ src/components/foundation/Colors/index.js
@@ -0,0 +1,9 @@
+const colors = {
+	white: "#FFFFFF",
+	black: "#121212",
+	gray: "#4b4b4b",
+	primary: "#6a1b9a",
+	secondary: "#2196f3",
+};
+
+export default colors;
---
+++ src/theme/index.js
@@ -0,0 +1,5 @@
+import colors from "../components/foundation/Colors";
+
+export default {
+	colors,
+};
