da365cf1e183d875811ca35dc6f8ad44d1a453d6
ReneSena <rene.sena3005@gmail.com>
2021-02-18 23:25:08 -0300

    Criação do componente de Reset.

---
+++ src/components/foundation/Reset/index.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+const GlobalStyle = createGlobalStyle`
+
+	* {
+		box-sizing: border-box;
+		text-decoration: none;
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+
+	${normalize}
+
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+		display: flex;
+		width: 100%;
+		min-height: 100vh;
+		font-family: 'Lato', sans-serif;
+
+	}
+
+	#__next {
+		width: 100%;
+	}
+`;
+
+export default GlobalStyle;
