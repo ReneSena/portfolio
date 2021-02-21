03fadfe86fb9488dff24b41544055e3c0ad6db39
ReneSena <rene.sena3005@gmail.com>
2021-02-21 20:30:45 -0300

    Adição de novas cores.

--- src/components/foundation/Colors/index.js
+++ src/components/foundation/Colors/index.js
@@ -1,7 +1,8 @@
 const colors = {
 	white: "#FFFFFF",
-	black: "#121212",
-	gray: "#4b4b4b",
+	grey: "#4b4b4b",
+	grey100: "#f5f5f5",
+	black: "#222",
 	primary: "#6a1b9a",
 	secondary: "#2196f3",
 };
