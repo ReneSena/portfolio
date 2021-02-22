16f9717eb5a1cbb8fac80c54ab59f40babb5aab7
ReneSena <rene.sena3005@gmail.com>
2021-02-21 23:55:08 -0300

    Adição do grid ao layout.

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,7 +1,9 @@
-import colors from "../components/foundation/Colors";
-import typographys from "../components/foundation/Typographys";
+import colors from '../components/foundation/Colors';
+import typographys from '../components/foundation/Typographys';
+import breakpoints from '../components/foundation/Breakpoints';
 
 export default {
 	colors,
 	typographys,
+	breakpoints,
 };
