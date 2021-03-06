dbbf462912cd3ef6bb145d92216ca356b48cddda
ReneSena <rene.sena3005@gmail.com>
2021-03-06 20:41:53 -0300

    Ajustando espaçamento entre as colunas.

--- src/components/commons/Projects/styles.js
+++ src/components/commons/Projects/styles.js
@@ -3,28 +3,17 @@ import { breakpointsMedia } from '../../../utils/breakpointsMedia';
 
 export const ListWrapper = styled.ul`
 	display: grid;
+	gap: 30px;
 	grid-template-columns: 1fr;
 
 	${breakpointsMedia({
 		md: css`
 			display: grid;
 			grid-template-columns: repeat(2, 1fr);
-			margin: 30px;
 		`,
 		xl: css`
 			display: grid;
 			grid-template-columns: repeat(3, 1fr);
-			margin: 30px;
 		`,
 	})}
-
-	& a {
-		margin-bottom: 30px;
-
-		${breakpointsMedia({
-			md: css`
-				margin: 30px;
-			`,
-		})}
-	}
 `;
