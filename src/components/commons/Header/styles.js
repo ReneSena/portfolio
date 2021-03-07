255fd3ff71cc7b40fe85b10bacab8870a79c7ea9
ReneSena <rene.sena3005@gmail.com>
2021-03-07 16:17:59 -0300

    Deixando Header Fixo.

--- src/components/commons/Header/styles.js
+++ src/components/commons/Header/styles.js
@@ -7,16 +7,19 @@ export const Container = styled.header`
 	align-items: center;
 	justify-content: space-between;
 	flex-direction: column;
-	z-index: 100;
 	transition: background 500ms linear;
 	background-color: #fff;
 	color: white;
+	position: sticky;
+	top: 0;
+	left: 0;
+	z-index: 100;
 
 	${(props) => {
 		if (props.isInitial) {
 			return css`
-				background-color: rgba(0, 0, 0, 0.2);
-				backdrop-filter: blur(10px);
+				background-color: rgba(255, 255, 255, 0.3);
+				backdrop-filter: blur(200px);
 			`;
 		}
 	}}
